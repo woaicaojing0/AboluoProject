4d02fbdaf32637ac647fd8c653ceaf12bdd47112
cj <1206067690@qq.com>
2016-09-23 23:20:33 +0800

    修修改改，增加图片，登录注册改为toobar

--- app/src/main/java/com/aboluo/com/RetrievepwdActivity.java
+++ app/src/main/java/com/aboluo/com/RetrievepwdActivity.java
@@ -2,17 +2,27 @@ package com.aboluo.com;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
+import android.widget.TextView;
 
 /**
  * Created by CJ on 2016/9/21.
  */
 
 public class RetrievepwdActivity  extends Activity{
+    private TextView retrievepwd_btn_close;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.retrievepwd);
+        retrievepwd_btn_close = (TextView) findViewById(R.id.retrievepwd_btn_close);
+        retrievepwd_btn_close.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                RetrievepwdActivity.this.finish();
+            }
+        });
     }
 }

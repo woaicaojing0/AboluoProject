c85e17f034dbba78b2948113810abbf309f750ef
cj <1206067690@qq.com>
2016-10-25 19:25:03 +0800

    图片点击放大

---
+++ app/src/main/java/com/aboluo/com/GoodsDetailImageActivity.java
@@ -0,0 +1,16 @@
+package com.aboluo.com;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by CJ on 2016/10/25.
+ */
+
+public class GoodsDetailImageActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_goods_detail_image);
+    }
+}

0a482f31ceb840c6f8d8cc45935a9a32725fea46
cj <1206067690@qq.com>
2016-11-23 23:21:38 +0800

    下单时，没有默认地址程序crash

--- app/src/main/java/com/aboluo/com/MakeOrderActivity.java
+++ app/src/main/java/com/aboluo/com/MakeOrderActivity.java
@@ -62,6 +62,7 @@ public class MakeOrderActivity extends Activity implements View.OnClickListener 
     private TextView order_yunfei;
     private double yunfei = 0.0;
     private int choose_address_requestcode =2;
+    private static int AddressId =0;
 private RelativeLayout change_make_sure_location;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -187,50 +188,59 @@ private RelativeLayout change_make_sure_location;
             bean1.setFreight(goodsShoppingCartListBean.get(i).getYunfei());
             bean.add(bean1);
         }
-        final Gson gson = new Gson();
-        final String Products = gson.toJson(bean);
-        pdialog.setTitleText("提交中");
-        pdialog.show();
-        StringRequest stringRequest = new StringRequest(Request.Method.POST, url + "/api/Order/SubOrder", new Response.Listener<String>() {
-            @Override
-            public void onResponse(String response) {
-                response = response.replace("\\", "");
-                response = response.substring(1, response.length() - 1);
-                BaseModel baseModel = new BaseModel();
-                baseModel = gson.fromJson(response, BaseModel.class);
-                if (baseModel.isIsSuccess()) {
-                    Intent intent = new Intent(MakeOrderActivity.this, OrderPayActivity.class);
-                    intent.putExtra("payMoney", lastmoney);
-                    intent.putExtra("OrderNum", baseModel.getOrderSerialId().toString());
-                    startActivityForResult(intent, requsetcode);
+        if(addressDefaultBean.isIsSuccess() == false&&AddressId==0)
+        {
+            Toast.makeText(this, "请选择收货地址", Toast.LENGTH_SHORT).show();
+        }else {
+            final Gson gson = new Gson();
+            final String Products = gson.toJson(bean);
+            pdialog.setTitleText("提交中");
+            pdialog.show();
+            StringRequest stringRequest = new StringRequest(Request.Method.POST, url + "/api/Order/SubOrder", new Response.Listener<String>() {
+                @Override
+                public void onResponse(String response) {
+                    response = response.replace("\\", "");
+                    response = response.substring(1, response.length() - 1);
+                    BaseModel baseModel = new BaseModel();
+                    baseModel = gson.fromJson(response, BaseModel.class);
+                    if (baseModel.isIsSuccess()) {
+                        Intent intent = new Intent(MakeOrderActivity.this, OrderPayActivity.class);
+                        intent.putExtra("payMoney", lastmoney);
+                        intent.putExtra("OrderNum", baseModel.getOrderSerialId().toString());
+                        startActivityForResult(intent, requsetcode);
+                        pdialog.dismiss();
+                    } else {
+                    }
+                    Toast.makeText(MakeOrderActivity.this, response, Toast.LENGTH_SHORT).show();
                     pdialog.dismiss();
-                } else {
                 }
-                Toast.makeText(MakeOrderActivity.this, response, Toast.LENGTH_SHORT).show();
-                pdialog.dismiss();
-            }
-        }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                Toast.makeText(MakeOrderActivity.this, error.toString(), Toast.LENGTH_SHORT).show();
-                byte[] htmlBodyBytes = error.networkResponse.data;
-                Log.i("woaicaojingeorror", new String(htmlBodyBytes));
-                pdialog.dismiss();
-            }
-        }) {
-            @Override
-            protected Map<String, String> getParams() throws AuthFailureError {
-                Map<String, String> map = new HashMap<>();
-                map.put("MemberId", "1");
-                map.put("TotalPrice", moeny);
-                map.put("AddressId", String.valueOf(addressDefaultBean.getResult().getId()));
-                map.put("Remark", edit_remark.getText().toString());
-                map.put("Products", Products);
-                map.put("APPToken", APPToken);
-                return map;
-            }
-        };
-        requestQueue.add(stringRequest);
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    Toast.makeText(MakeOrderActivity.this, error.toString(), Toast.LENGTH_SHORT).show();
+                    byte[] htmlBodyBytes = error.networkResponse.data;
+                    Log.i("woaicaojingeorror", new String(htmlBodyBytes));
+                    pdialog.dismiss();
+                }
+            }) {
+                @Override
+                protected Map<String, String> getParams() throws AuthFailureError {
+                    Map<String, String> map = new HashMap<>();
+                    map.put("MemberId", "1");
+                    map.put("TotalPrice", moeny);
+                    if(addressDefaultBean.isIsSuccess()) {
+                        map.put("AddressId", String.valueOf(addressDefaultBean.getResult().getId()));
+                    }else {
+                        map.put("AddressId",String.valueOf(AddressId));
+                    }
+                    map.put("Remark", edit_remark.getText().toString());
+                    map.put("Products", Products);
+                    map.put("APPToken", APPToken);
+                    return map;
+                }
+            };
+            requestQueue.add(stringRequest);
+        }
     }
 
     @Override
@@ -271,6 +281,7 @@ private RelativeLayout change_make_sure_location;
                     address_detailaddress.setText(stringBuffer.toString());
                     address_name.setText(memberAddressListBean.get(0).getReceiver().toString());
                     address_phone.setText(memberAddressListBean.get(0).getMobile().toString());
+                    AddressId = memberAddressListBean.get(0).getId();
                 }
                 break;
             default:
